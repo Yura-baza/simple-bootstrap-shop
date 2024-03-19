d5e4cf606cc0d793e8c5026f024cff636e09cbab
Yura-baza <65945436+Yura-baza@users.noreply.github.com>
2024-03-19 14:18:26 +0500

    output-review.php

--- includes/output-review.php
+++ includes/output-review.php
@@ -6,21 +6,21 @@ rsort($reviewfiles);
 
 ?>
 <div class="card card-outline-secondary my-4">
-    <div class="card-header">Product Reviews</div>
+    <div class="card-header">Обзоры товаров</div>
     <div class="card-body review-body">
 <?php
 foreach($reviewfiles as $reviewfile) {
 	$lines = file($reviewfile, FILE_IGNORE_NEW_LINES);// filedata into an array
 		
 	$review_id = $lines[0]; // id
-	$date = $lines[1]; // date
-	$name = $lines[2]; // name
-	$message = $lines[3]; // message
+	  $date = $lines[1]; // date
+	  $name = $lines[2]; // name
+	  $message = $lines[3]; // message
 
     // loop for name, message and date
 ?>		
 		<p><?php echo $message; ?></p>
-		<small class="text-muted"><?php echo 'Posted by <b>'. $name .'</b> on '.$date; ?></small>
+		<small class="text-muted"><?php echo 'Написал пользователь <b>'. $name .'</b> на '.$date; ?></small>
 	
 		<?php if($admin) { ?>
 		<!-- DELETE BUTTON -->
@@ -29,12 +29,11 @@ foreach($reviewfiles as $reviewfile) {
 			<input type="hidden" class="form-control" name="review_dir" value="<?php echo $review_dir; ?>" />	<!-- review_dir is dir name -->
 			<input type="hidden" class="form-control" name="review_id" value="<?php echo $review_id; ?>" />	<!-- id of review file -->
 			<input type="hidden" class="form-control" name="name" value="<?php echo $name; ?>" />	<!-- name only for echo -->						
-			<button class="guest-delete btn btn-danger m-1" type="submit" name="submit">Delete</button>				
+			<button class="guest-delete btn btn-danger m-1" type="submit" name="submit">Удалить</button>				
 		</form>
 		<!-- EDIT; Trigger the modal with a button -->
-		<button type="button" class="guest-edit btn btn-secondary float-right m-1" data-toggle="modal" data-target="#<?php echo $review_id.'-edit'; ?>">Edit</button>
-		<?php
-		include 'includes/modal-edit-review.php';
+		<button type="button" class="guest-edit btn btn-secondary float-right m-1" data-toggle="modal" data-target="#<?php echo $review_id.'-edit'; ?>">Редактировать</button>
+		<?php include 'includes/modal-edit-review.php';
 		echo '<br /><br />';		
 		} // endif admin
 		
@@ -45,7 +44,7 @@ foreach($reviewfiles as $reviewfile) {
 		<div class="card-footer">
 <!-- Modal button Review-->
 <?php if(!$admin) { ?>
-		<button type="button" class="btn btn-success review-file float-left" data-toggle="modal" data-target="#<?php echo $file_id.'-review'; ?>">Leave a Review</button>
+		<button type="button" class="btn btn-success review-file float-left" data-toggle="modal" data-target="#<?php echo $file_id.'-review'; ?>">Оставить отзыв</button>
 <?php include 'includes/modal-review.php'; } ?>
 
 	</div>
